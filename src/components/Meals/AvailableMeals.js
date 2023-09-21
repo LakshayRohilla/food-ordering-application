b29bfd3218dee519ad50496927ac1c7ec98d2102
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-09-21 17:07:53 +0530

    displaying individual meal item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css'
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 export default function AvailableMeals() {
     const DUMMY_MEALS = [
@@ -32,7 +34,10 @@ export default function AvailableMeals() {
 
     return (
         <section className={classes.meals}>
-            <ul>{DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>)}</ul>
+            <Card>
+                <ul>{DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)} </ul>
+            </Card>
+
         </section>
     );
 
